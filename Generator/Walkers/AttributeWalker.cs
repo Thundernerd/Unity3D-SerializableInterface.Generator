6ed01e613c7fbaba8fb6382fb39285b71b61dbcf
Christiaan Bloemendaal <github@tnrd.net>
2023-01-08 20:07:42 +0100

    refactor: updated project to work properly within Unity

---
+++ Generator/Walkers/AttributeWalker.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TNRD.SerializableInterface.Generators.Walkers
+{
+    internal class AttributeWalker : CSharpSyntaxWalker
+    {
+        public bool HasSerializableInterfaceAttribute { get; private set; }
+            
+        /// <inheritdoc />
+        public override void VisitAttribute(AttributeSyntax node)
+        {
+            if (node.Name.ToString() == "SerializableInterface") // TODO: Make this change-proof
+            {
+                HasSerializableInterfaceAttribute = true;
+            }
+        }
+    }
+}
---
+++ Generator/Walkers/InterfaceWalker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TNRD.SerializableInterface.Generators.Walkers
+{
+    internal class InterfaceWalker : CSharpSyntaxWalker
+    {
+        private readonly List<InterfaceDeclarationSyntax> interfaces = new List<InterfaceDeclarationSyntax>();
+
+        public IReadOnlyList<InterfaceDeclarationSyntax> Interfaces => interfaces;
+
+        /// <inheritdoc />
+        public override void VisitInterfaceDeclaration(InterfaceDeclarationSyntax node)
+        {
+            AttributeWalker walker = new AttributeWalker();
+
+            foreach (AttributeListSyntax attributeListSyntax in node.AttributeLists)
+            {
+                walker.Visit(attributeListSyntax);
+            }
+
+            if (walker.HasSerializableInterfaceAttribute)
+            {
+                interfaces.Add(node);
+            }
+        }
+    }
+}
